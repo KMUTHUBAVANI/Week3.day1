a2dfb8e8b1157ba451cdef3ea6216df3d5d054c6
KMUTHUBAVANI <40690083+KMUTHUBAVANI@users.noreply.github.com>
2022-01-15 14:20:10 +0530

    Update Student.java

--- student/Student.java
+++ student/Student.java
@@ -1,1 +1,31 @@
+package week3.day1.student;
+
+import week3.day1.department.*;
+
+public class Student extends Department{
+	
+	public void studentName() {
+		System.out.println("Student name is Ram Prasad");
+	}
+	public void studentDept() {
+		System.out.println("Student Department is IT");
+	}
+	public void studentId() {
+		System.out.println("Student Id is 03UM36");
+	}
+	
+	public static void main(String args[]) {
+		
+		Student student = new Student();
+		student.collegeCode();
+		student.collegeName();
+		student.collegeRank();
+		student.deptName();
+		student.studentName();
+		student.studentDept();
+		student.studentId();
+		
+	}
+
+}
 
