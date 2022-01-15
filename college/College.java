2b68ae89d0786d259c88643af5c54a90fc54fdab
KMUTHUBAVANI <40690083+KMUTHUBAVANI@users.noreply.github.com>
2022-01-15 14:12:52 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,15 @@
+package week3.day1.college;
+
+public class College {
+	
+	public void collegeName() {
+		System.out.println("College name is MIT");
+	}
+	public void collegeCode() {
+		System.out.println("College code is 012");
+	}
+	public void collegeRank() {
+		System.out.println("College rank is 1st");
+	}
+
+}
